bbc10dbc14cdd4f48be3279614a6bec776cde780
JCharlieDev <al15211330@ite.edu.mx>
2020-08-13 01:37:12 -0700

    Assets
    Added some assets

--- EscapeProject/Source/EscapeProject/NewActorComponent.cpp
+++ EscapeProject/Source/EscapeProject/NewActorComponent.cpp
@@ -2,6 +2,7 @@
 
 
 #include "NewActorComponent.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UNewActorComponent::UNewActorComponent()
@@ -19,9 +20,29 @@ void UNewActorComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("This is a Warning!!"));
-	UE_LOG(LogTemp, Error, TEXT("This is an ERROR!!"));
-	UE_LOG(LogTemp, Display, TEXT("This is a Display!!"));
+	//FString Log = TEXT("Hello!");
+	//FString* PrtLog = &Log;	//	This is a pointer
+
+	//Log.Len();	//	Direct haccess to the member
+
+	////*PrtLog <- //	De-referencing has a lower precedence than the dot operator. = (*PrtLog).someStuff
+	//PrtLog->Len();	//	Indirect access to the member
+
+	////UE_LOG(LogTemp, Warning, TEXT("%s"), *Log);	//	This is an overloaded operator
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), **PrtLog);
+
+	////UE_LOG(LogTemp, Display, TEXT("This is a Display!!"));
+	////UE_LOG(LogTemp, Error, TEXT("This is an ERROR!!"));
+
+	FString ActorName = GetOwner()->GetName();
+	FString* PtrActorName = &ActorName;
+
+	FString ObjectPosition = GetOwner()->GetActorLocation().ToString();
+	//FString ObjectPosition = GetOwner()->GetActorTransform().ToString();	//	Returns rotation and scalee
+
+	UE_LOG(LogTemp, Error, TEXT("Object Name Is: %s"), **PtrActorName);			//	Both ways work, one just let's you use the name in other places.
+	//	UE_LOG(LogTemp, Error, TEXT("Object Name Is: %s"), *GetOwner()->GetName());
+	UE_LOG(LogTemp, Warning, TEXT("%s Position is: %s "), **PtrActorName, *ObjectPosition);
 	
 }
 
